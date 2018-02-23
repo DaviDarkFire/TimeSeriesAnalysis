d95c99a99a5b223fb1d2d5e314dd87dd227d5d6a
DaviDarkFire <daviferreirasantog@gmail.com>
2018-02-23 12:48:51 -0400

    Teste da biblioteca DTW

--- prediction.py
+++ prediction.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import mlpy
+import numpy as np
+from scipy.spatial.distance import euclidean
+from fastdtw import fastdtw
+
+def main():
+    x = []
+    y = []
+    for i in range(0,10,1):
+        x.append(i)
+        y.append(i)
+    x = np.array(x)
+    y = np.array(y)
+    distance, path = fastdtw(x, y, dist=euclidean)
+    print(distance)
+main()
