15290d46ce00c19f3a11f5262d1e3c3110ae8902
Davi Ferreira Santo <davi@davi.davi>
2018-03-14 00:06:20 -0400

    Knn em andamento

--- knn_prediction.py
+++ knn_prediction.py
@@ -4,34 +4,34 @@ import numpy as np
 from scipy.spatial.distance import euclidean
 from fastdtw import fastdtw
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 
 def trataValores(valores): #transforma os literais em valores inteiro e float, respectivamente, pra uso posterior
     return int(valores[0]), float(valores[1])
 
 def main():
-    serie = []
+    x = []
+    y = []
     with open('output.ou') as f: #inicializa os valores de W a partir do arquivo de entrada
         for linha in f:
             linha = linha.strip()
             if linha:
                 valores = linha.split(',')
-                x,y = trataValores(valores)
+                a,b = trataValores(valores)
+                x.append(a)
+                y.append(b)
                 # cel = []
                 # cel.append(x)
                 # cel.append(y)
-                serie.append(y)
+                # serie.append(cel)
 
-    serie = np.matrix(serie)
-    fim_x = int(len(serie)*0.5)
-
-    x = serie[0:fim_x]
-    y = serie[fim_x:len(serie)-1] 
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=4)
-    logreg = LogisticRegression()
-    logreg.fit(X_train, y_train)
-    y_pred = logreg.predict(X_test)
-    # print metrics.accuracy_score(y_test, y_pred)
+    x = np.array(x)
+    x = x.reshape(x.shape[0],1)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=4)
+    knn = KNeighborsClassifier(n_neighbors=1)
+    knn.fit(x_train, y_train)
+    pred = knn.predict(x_test.reshape(x_test))
+    print accuracy_score(y_test.reshape(y_test))
 main()
