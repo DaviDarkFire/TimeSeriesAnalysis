2b322c1e412d59ac7db3b76129d3ae8886c88e45
Davi Ferreira Santo <davi@davi.davi>
2018-03-20 12:07:58 -0400

    Salvando alterações antes de testar o knn com dtw

---
+++ dtw.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from fastdtw import fastdtw
+import numpy as np
+from scipy.spatial.distance import euclidean
+
+def dtw(array1,array2):
+	# x = np.array([1,2,3,4,5,6,7,8,9,10])
+	# y = np.array([2,4,1,22,5,8,3,5,7,2])
+	distance, path = fastdtw(array1, array2, dist=euclidean)
+	return distance
+dtw()
--- knn_prediction.py
+++ knn_prediction.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from scipy.spatial.distance import euclidean
-from fastdtw import fastdtw
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neighbors import DistanceMetric
 from sklearn import preprocessing
-
+from dtw import dtw
 
 def trataValores(valores): #transforma os literais em valores inteiro e float, respectivamente, pra uso posterior
     return int(valores[0]), float(valores[1])
@@ -40,7 +40,7 @@ def main():
     x = x.reshape(x.shape[0],1)
     y = np.array(y)
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=4)
-    knn = KNeighborsRegressor(n_neighbors=1)
+    knn = KNeighborsRegressor(n_neighbors=1, metric='pyfunc', func=dtw)
     knn.fit(x_train, y_train)
     pred = knn.predict(x_test)
     return x_test, y_test, pred
