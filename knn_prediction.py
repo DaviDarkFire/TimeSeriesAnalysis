0adc666c780f260cf37b77e7f2e60b5a98148f70
Davi Ferreira Santo <davi@davi.davi>
2018-04-13 00:48:12 -0400

    KNN quase arrumado

--- knn_prediction.py
+++ knn_prediction.py
@@ -4,6 +4,8 @@ import numpy as np
 from scipy.spatial.distance import euclidean
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.neighbors import DistanceMetric
 from sklearn import preprocessing
 from dtw import dtw
@@ -20,27 +22,28 @@ def main():
             if linha:
                 valores = linha.split(',')
                 a,b = trataValores(valores)
-                x.append(a)
                 y.append(b)
-
-    # x = np.zeros((len(x_aux), 3))
-    # for i, val in enumerate(x_aux):
-    #     x[i][2] = x_aux[i]%100
-    #     # print x_aux[i]%100
-    #     x_aux[i] /= 100 
-    #     x[i][1] = x_aux[i]%100
-    #     # print x_aux[i]%100
-    #     x_aux[i] /= 100
-    #     x[i][0] = x_aux[i]
-    #     # print x_aux[i]
-    #     # print "\n"
-    # np.set_printoptions(threshold='nan')
     
-    x = np.array(x)
-    x = x.reshape(x.shape[0],1)
-    y = np.array(y)
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=4)
-    knn = KNeighborsRegressor(n_neighbors=1, metric=dtw)
-    knn.fit(x_train, y_train)
-    pred = knn.predict(x_test)
-    return x_test, y_test, pred
+    y_aux = []
+    x_aux = []
+    count = 0;
+    cel = []
+    for i in y:
+        count += 1
+        if (count % 5 == 0 and count != 0):
+            y_aux.append(i)
+            x_aux.append(cel)
+            cel = []
+        else:
+            cel.append(i)
+
+        
+    print np.array(x_aux)
+    # print y_aux
+    # y = np.array(y)
+    # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=4)
+    # knn = KNeighborsRegressor(n_neighbors=1, metric=dtw)
+    # knn.fit(x_train, y_train)
+    # pred = knn.predict(x_test)
+    # return x_test, y_test, pred
+main()
